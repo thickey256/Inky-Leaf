979182227c1d4492da22f5798d7b540cfb4a2228
thickey256 <pi@inkyleaf.local>
2019-05-04 21:27:16 +0100

    Added Inky What output

--- leaf_image.py
+++ leaf_image.py
@@ -1,5 +1,10 @@
 from PIL import Image, ImageDraw, ImageFont
+from inky import InkyWHAT
 import datetime
+
+# setup inky for display
+inky_display = InkyWHAT("red")
+inky_display.set_border(inky_display.RED)
 
 # get an image
 base = Image.open('img/base.png').convert('RGBA')
@@ -15,10 +20,12 @@ fnt_small = ImageFont.truetype('fonts/roboto/Roboto-Regular.ttf', 20)
 # get a drawing context
 d = ImageDraw.Draw(txt)
 
-charge_percent = 49
+# Temp vars until I can hook up the Nissan API
+# I need to get a leaf first
+charge_percent = 1
 charging = 1
-charge_eta = '1h 35m'
-distance = '45 Miles'
+charge_eta = '3h 35m'
+distance = '1 Miles'
 
 #Which battery image do we want?
 if charge_percent < 10:
@@ -42,7 +49,7 @@ if charging == 1:
     clock = Image.open('img/clock.png').convert('RGBA')
     charge = Image.open('img/charging.png').convert('RGBA')
     charge = Image.alpha_composite(clock, charge)
-    
+
     # add charge eta to text layer
     d.text((80,125), str(charge_eta), font=fnt, fill=(0,0,0,255))
 
@@ -63,5 +70,13 @@ if charging == 1:
 #Merge the text layer
 out = Image.alpha_composite(out, txt)
 
+# Convert the image to use a white / black / red colour palette
+pal_img = Image.new("P", (1, 1))
+pal_img.putpalette((255, 255, 255, 0, 0, 0, 255, 0, 0) + (0, 0, 0) * 252)
 
-out.show()
+# Makes our image work with our pallet
+img = out.convert("RGB").quantize(palette=pal_img)
+
+# Displays the image
+inky_display.set_image(img)
+inky_display.show()
